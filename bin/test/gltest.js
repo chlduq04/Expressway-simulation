7aad2f6b1b6f3ed8063b454ccece2ead08c9b325
chlduq04 <chlduq04@gmail.com>
2013-07-27 22:36:07 +0900

    201307272219

--- bin/test/gltest.js
+++ bin/test/gltest.js
@@ -1,8 +1,8 @@
 var gl;
+var scene;
 window.onload = function() {
 	gl = $("#container").WebGL();
 	gl.init();
-	gl.test();
 };
 if(jQuery)(function($){
 	$.extend($.fn, {
@@ -11,9 +11,12 @@ if(jQuery)(function($){
 			var defaults = {
 					width :  800,
 					height : 600,
-					camerax : -20,
-					cameray : 5,
-					cameraz : 0,
+					camera_x : -20,
+					camera_y : 5,
+					camera_z : 0,
+					position_x : 10,
+					position_y : 0,
+					position_z : 0,
 					key_right : "D",
 					key_left : "A",
 					key_down : "S",
@@ -21,35 +24,52 @@ if(jQuery)(function($){
 					key_break : "M"
 			};$.extend(defaults, opt);
 
-			var renderer, scene;
+			/** System value **/
+
+			var renderer;
 			var camara, light;
 			var target= new THREE.Vector3(), lon = 90, lat = 0, phi = 0, theta = 0;
 			var isUserInteracting = false;
 			var onPointerDownPointerX,onPointerDownPointerY;
 			var onPointerDownLon,onPointerDownLat;
-
-			var currentlyPressedKeys = {};
-			var camera_lookat = { x : 0, y : 0, z : 0 };
-			var camera_position = { x : defaults.camerax, y : defaults.cameray, z : defaults.cameraz };
 			var simulation_speed = 20;
+			var currentlyPressedKeys = {};
+
+			/** Simulation value **/
+
+			var camera_lookat = { x : 0, y : 0, z : 0 };
+			var camera_position = { x : defaults.camera_x, y : defaults.camera_y, z : defaults.camera_z };
+			var m_position = { x : defaults.position_x, y : defaults.position_y, z : defaults.position_z };
 
 			var m_speed = { x : 0, y : 0, z : 0 };
 			var m_limitspeed = { x : 50, y : 50, z : 50 };
-			var m_position = { x : defaults.camerax, y : defaults.cameray, z : defaults.cameraz };
-			var o_positions = [];
+			
+			/** Other value **/
+			
+			var road_object;
+			var road_position = { x : 0, y : -2.5, z : 0 };
+			var road_scale = { x : 100, y : 0.2,z : 20 };
+
+			/** Car value **/
+
+			var o_positions = {};
 
 			/** Initialize **/
+
 			this.init = function(){
 				self.settingRender( defaults.width, defaults.height )
-				self.settingCamera( defaults.camerax, defaults.cameray, defaults.cameraz, target )
+				self.settingCamera( defaults.camera_x, defaults.camera_y, defaults.camera_z, target )
 				self.settingLight( 0xFFFF00, 10, 20, 10 );
-				renderer.render( scene, camera );
 				self.settingKey();
 				self.settingMouse();
+				self.drawCar( m_position, 0xFF0000, 0 );
+				self.drawLoad( road_position, road_scale, "roada" );
+//				self.drawCar( {x:10,y:0,z:0}, 0xFF0000, 0 );
+				renderer.render( scene, camera );
 			},
 
-
 			/** Setting **/
+
 			this.settingRender = function( width, height ){
 				renderer = new THREE.WebGLRenderer();
 				scene = new THREE.Scene();
@@ -96,10 +116,6 @@ if(jQuery)(function($){
 						if(isUserInteracting){
 							
 						}else{
-							m_position.x += m_speed.x
-							m_position.y += m_speed.y
-							m_position.z += m_speed.z
-							self.controlCamera( m_position.x, m_position.y, m_position.z, camera_lookat );
 							renderer.render( scene, camera );
 						}
 						myLoop();
@@ -108,14 +124,13 @@ if(jQuery)(function($){
 				myLoop();
 			},
 
-
 			/** Control **/
 			this.controlLight = function(){
 				light = new THREE.PointLight( color );
 				light.position.set( x, y, z );
 				scene.add( light );
 			},
-			
+
 			this.controlCamera = function( posx, posy, posz, lookat ){
 				camera.position.set( posx, posy, posz );
 				camera.lookAt( lookat );
@@ -123,30 +138,57 @@ if(jQuery)(function($){
 			
 			this.controlKeyDown =  function (event) {
 				currentlyPressedKeys[event.keyCode] = true;
+				var press = 0;
 				if( String.fromCharCode(event.keyCode) == defaults.key_left ){
-					camera_lookat.z -= 0.1;
-					self.controlCamera( m_position.x, m_position.y, m_position.z, camera_lookat );
+					press += 1;
 				}
 				if( String.fromCharCode(event.keyCode) == defaults.key_right ){
-					camera_lookat.z += 0.1;
-					self.controlCamera( m_position.x, m_position.y, m_position.z, camera_lookat );
+					press += 10;
 				}
 				if( String.fromCharCode(event.keyCode) == defaults.key_up ){
-					m_speed.x += 0.001;
-					self.controlCamera( m_position.x, m_position.y, m_position.z, camera_lookat );
+					press += 100;
 				}
 				if( String.fromCharCode(event.keyCode) == defaults.key_down ){
-					m_speed.x -= 0.001;
-					self.controlCamera( m_position.x, m_position.y, m_position.z, camera_lookat );
+					press += 1000;
 				}
+				switch(press){
+				case 1:
+					m_position.z -= 0.2;
+					break;
+				case 10:
+					m_position.z += 0.2;
+					break;
+				case 100:
+					m_position.x += 0.2;
+					break;
+				case 101:
+					m_position.x += 0.2;
+					m_position.z -= 0.2;
+					break;
+				case 110:
+					m_position.x += 0.2;
+					m_position.z += 0.2;
+					break;
+				case 1000:
+					m_position.x -= 0.2;
+					break;
+				case 1001:
+					m_position.x -= 0.2;
+					m_position.z -= 0.2;
+					break;
+				case 1010:
+					m_position.x -= 0.2;
+					m_position.z += 0.2;
+					break;
+				}
+				self.drawCar(m_position, "", 0);
 				renderer.render( scene, camera );
 			},
+			
 			this.controlKeyUp = function(event) {
 				currentlyPressedKeys[event.keyCode] = false;
 			},
-			
-			
-			
+
 			this.controlMouseDown = function(event){
 				event.preventDefault();
 				isUserInteracting = true;
@@ -157,6 +199,7 @@ if(jQuery)(function($){
 				onPointerDownLon = lon;
 				onPointerDownLat = lat;
 			},
+			
 			this.controlMouseMove = function(event){
 				if ( isUserInteracting ) {
 					lon = ( onPointerDownPointerX - event.clientX ) * 0.1 + onPointerDownLon;
@@ -174,20 +217,44 @@ if(jQuery)(function($){
 					renderer.render( scene, camera );
 				}
 			},
+			
 			this.controlMouseUp = function(event){
 				isUserInteracting = false;
 			},
+			
 			this.controlMouseOut = function(){
-				
+				isUserInteracting = false;
 			},
 			
+			this.drawCar = function( position, color, name ){
+				var target = o_positions[name];
+				if( target == undefined ){
+					var geometry = new THREE.CubeGeometry( 0.2, 5, 5 );
+					var material = new THREE.MeshLambertMaterial( { color: color } );
+					var mesh = new THREE.Mesh( geometry, material );
+					mesh.name = name;
+					mesh.position.x = position.x;
+					mesh.position.y = position.y;
+					mesh.position.z = position.z;
+					o_positions[name] = mesh;
+					scene.add( mesh );
+				}else{
+					target.position.x = position.x;
+					target.position.y = position.y;
+					target.position.z = position.z;
+				}
+			},
 			
-			this.test = function(){
-				var geometry = new THREE.CubeGeometry( 5, 5, 5 );
-				var material = new THREE.MeshLambertMaterial( { color: 0xFF0000 } );
+			this.drawLoad = function( position, scale, name ){
+				var geometry = new THREE.CubeGeometry( scale.x, scale.y, scale.z );
+				var material = new THREE.MeshLambertMaterial( { color: 0x888888 } );
 				var mesh = new THREE.Mesh( geometry, material );
+				mesh.name = name;
+				mesh.position.x = position.x;
+				mesh.position.y = position.y;
+				mesh.position.z = position.z;
+				o_positions[name] = mesh;
 				scene.add( mesh );
-				renderer.render( scene, camera );
 			}
 			return this;
 		},
